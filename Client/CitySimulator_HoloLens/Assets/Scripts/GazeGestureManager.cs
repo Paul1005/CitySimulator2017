20eab789c84cd2bbcb746a5aecba8bbe058a2180
Paul1005 <mccarliep@gmail.com>
2017-11-01 18:59:58 -0700

    First client-hololens prototype

---
+++ Client/CitySimulator_HoloLens/Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour
+{
+    public static GazeGestureManager Instance { get; private set; }
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+    int tapCount = 0;
+    // Use this for initialization
+    void Awake()
+    {
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            this.tapCount = tapCount;
+
+            //this is to try and stop the program from getting confused between spacial mapping and rotation gestures
+            if (this.tapCount == 2)
+            {
+                print("tapcount:" + this.tapCount);
+                print(FocusedObject);
+                // Send an OnSelect message to the focused object and its ancestors.
+                if (FocusedObject != null)
+                {
+                    FocusedObject.SendMessageUpwards("OnSelect", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        };
+
+        //similar to above version, could maybe try and refactor so repetition is not needed
+        if(this.tapCount == 2)
+        {
+            recognizer.StartCapturingGestures();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Figure out which hologram is focused this frame.
+        GameObject oldFocusObject = FocusedObject;
+
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            FocusedObject = hitInfo.collider.gameObject;
+           // print("hit");
+        }
+        else
+        {
+            // If the raycast did not hit a hologram, clear the focused object.
+            FocusedObject = null;
+            //print("miss");
+        }
+
+        // If the focused object changed this frame,
+        // start detecting fresh gestures again.
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
---
+++ Client/CitySimulator_HoloLens/README.md
@@ -0,0 +1,29 @@
+## Capstone17
+Created by 2017-10-03
+
+Client side Unity Project
+
+City Simulator Client side
+- To contribute, fork this repo please
+
+How to play simulator prototype.
+
+In-Main menu
+- Play button: starts simulation
+- Exit: terminates simulation
+
+
+In-simulation
+- ASWD keys: move camera
+    - A: Left
+    - W: Forward
+    - S: Back
+    - D: Right
+- Mouse moving: change camera direction
+- Reload button: reload entire scene
+- quit Button: back to main menu
+
+Issues & Bugs
+-  Windows build
+    - WASD deosn't work properly
+- GUI Overlapped by world objects when zoom in too much
